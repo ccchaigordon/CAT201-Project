c66a07097369e8b1d47907554f96588ae262107a
MeiZhen <meizhenwong2004@gmail.com>
2024-12-27 22:32:04 +0800

    Added contact info at checkout, modified css

--- frontend/src/components/CheckOut/CheckOutPage.tsx
+++ frontend/src/components/CheckOut/CheckOutPage.tsx
@@ -1,6 +1,19 @@
-//import React from "react";
+import {useState} from "react";
+import ContactInfoForm from "./ContactInfo";
+import "../../style/CheckOut.css";
 
 function CheckOutPage(){
+    const [contactInfo, setContactInfo] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        address: "",
+        postcode: "",
+        city: "",
+        state: "",
+    });
+
 return(
     <div className = "checkout-page">
         <div className="logo">
@@ -8,8 +21,13 @@ return(
                 <img src="/assets/logo.svg" alt="Logo" />
             </a>
         </div>
+        <div className="checkout-form-container">
+            <ContactInfoForm 
+            contactInfo={contactInfo} 
+            setContactInfo={setContactInfo}/>
+        </div>
     </div>
-);
+    );
 }
 
 export default CheckOutPage;
